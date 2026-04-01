ef45f54c86af09e1b39a1b30fcf3fbd6b0b323ca
Linus-llm <linusmolgaard@gmail.com>
2026-04-01 19:11:34 +0200

    second

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,14 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Javalin app = Javalin.create(config -> {
+            config.showJavalinBanner = false;
+        }).start(7070);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        app.get("/", ctx -> ctx.result("Server is running 🚀"));
+        app.get("/health", ctx -> ctx.json("{\"status\":\"OK\"}"));
     }
 }
